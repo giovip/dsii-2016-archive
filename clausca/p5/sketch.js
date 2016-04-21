aa81f5008728f2ee7468d93cefc538dd50c184d1
clausca <cla1242@gmail.com>
2016-04-21 16:08:14 +0200

    Added files via upload

--- clausca/p5/sketch.js
+++ clausca/p5/sketch.js
@@ -1,35 +1,97 @@
-var w = 16;
-var h = 16;
-var index = 0;
+//Clausca variazione 10 print in p5.js// 21.04.2016
+
+var x = 0;  
+var y = 40;  
+var g = 30; 
+
 
 
 function setup() {
-  createCanvas(640, 384);
-  background (0,0,100);
-  stroke(224);
-  strokeWeight(3);
-  smooth()
-  
-}
+   smooth(); 
+   createCanvas(800, 600); 
+ }
 
 function draw() {
-var x1 = w*index;
-var x2 = x1 + w;
-var y1 = h*23;
-var y2 = h*24;
-  if (random(2) < 1) {
-line(x2, y1, x1, y2);
-} else {
-line(x1, y1, x2, y2);
-}
- index++;
- 
-if (index == width/w) {
+    fill(0);
+    noStroke();
+    rect(0, 0, width, 40);
 	
-var immagine=get(0, h, width, h*23);
+	var font;
+	textSize(g/2);
+	noStroke();
+	fill('#FF00FF'); 
+	textSize(g/2);
+	//font = loadFont('Commodore Rounded v1.2.ttf');
+	textFont("Courier");
+    text('10 PRINT CHR$(205.5+RND(1)); : GOTO 10',10, g-10);
+	
 
-background(0,0,100);
-set(0, 0, immagine);
-index = 0;
+    var strokeW = g/4;
+    strokeWeight(strokeW);
+    strokeCap(SQUARE);
+    strokeJoin(BEVEL);
+	
+    RigheNere(); //fa una riga nera orizzontale che segue il disegno
+
+ 
+  if (random(2) < 1) {
+    line(x, y, x + g, y + g);  //disegno linea (\)
+  } else {
+    line(x + g, y, x, y + g); //disegno linea (/)
+  }
+  x = x + g;
+
+ 
+  if (x >= width) {   
+  //se supera la larghezza della pagina, va a capo e ricomincia da zero_va alla riga seguente
+    x = 0; 
+    y = y + g;
+	
+  }
+  
+  if (y >= height) {  
+  //se supera l'altezza della pagina, va a capo e ricomincia da zero con griglia random e background con opacità(funzione 'Cambio')
+    y = 40; 
+    Cambio();		
+  }
 }
+
+function Cambio() {
+  g=30+random(width);
+  //rettangolo senza contorni//
+  noStroke();
+  //opacità
+  fill(255,100); 
+  //rettangolo grande tutta l'altezza e larghezza della pagina
+  rect(x, y, width, height); 
+}
+
+
+function RigheNere () {
+  fill(0);
+  noStroke();
+  rect(x, y, width, g); //riga nera orizzontale 
+  stroke(random(0, 255), (0, 255), (0, 255));
+}
+
+
+function mousePressed() {
+  //se premo il mouse, ricomincio da x=0 con griglia e background differente
+  g=30;
+  x=0;
+  y=40;
+   //colori background random 
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  noStroke();
+  rect(x, y, width, height);
+
+}
+
+
+function keyTyped() { 
+// se premo 'n' salvo l'immagine
+
+ if (key === 'n') {
+    saveCanvas();
+  }
 }
